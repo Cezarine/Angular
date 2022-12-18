5b1b5ceedb231ed39082042bec995940135bf6dc
Cezarine <gcezarines@gmail.com>
2022-12-18 13:33:15 -0300

    primeiro modulo

---
+++ AluraPic/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  titleOM = 'OM Sistemas';
+  description = 'OM Sistemas';
+  url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9pgCqyBAl7d_HfPIStbKGMD8Ujl4-oijlw&usqp=CAU';
+}
